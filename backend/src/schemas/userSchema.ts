c752613e9c1b22131d5ba791a1f0254135a712fb
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2026-02-16 04:19:09 +0100

    update register schema

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -2,7 +2,13 @@ import * as z from "zod";
 
 export const registerSchema = z.object({
   email: z.email("Invalid email format"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])/,
+      "Password must contain at least one letter, one number, and one special character",
+    ),
 });
 
 export const loginSchema = z.object({
