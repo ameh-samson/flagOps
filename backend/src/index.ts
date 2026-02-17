e5f896cb6d432777f24d784486c56b0d91aa7600
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2026-02-17 20:24:11 +0100

    accept request from frotend link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,10 @@ const app: Express = express();
 const PORT = envProduction.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: envProduction.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/v1/api/auth", authRouter);
--- backend/src/utils/envProduction.ts
+++ backend/src/utils/envProduction.ts
@@ -6,4 +6,5 @@ export const envProduction = {
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET_KEY as string,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as StringValue,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 } as const;
