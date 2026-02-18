96767ed8e9b860f690c918e5ec6a0acde8866fb9
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2026-02-18 13:57:45 +0100

    add catch all route to redirect to /login

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,12 +2,13 @@ import LoginContainer from "@/components/screens/login/LoginContainer";
 import ProtectedLayout from "@/layout/ProtectedLayout";
 import PublicLayout from "@/layout/PublicLayout";
 import Dashboard from "@/screens/Dashboard";
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<PublicLayout />}>
+        <Route index path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginContainer />} />
       </Route>
 
